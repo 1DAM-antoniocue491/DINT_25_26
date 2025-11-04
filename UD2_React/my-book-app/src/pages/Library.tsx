775b908397847a9c2ccb81d88d26f3e0fe675f7c
unknown <2DAM-acuelop369@IESMARTINEZM.ES>
2025-11-04 19:50:08 +0100

    Actualizació

--- UD2_React/my-book-app/src/pages/Library.tsx
+++ UD2_React/my-book-app/src/pages/Library.tsx
@@ -7,7 +7,7 @@ interface LibraryProps {
     books: IBooks[]
 }
 
-export default function Library({books} : LibraryProps) {
+export default function Library({ books }: LibraryProps) {
     const [book, setBook] = React.useState<number>(0);
 
     const [states] = React.useState<string[]>(getState());
@@ -15,13 +15,18 @@ export default function Library({books} : LibraryProps) {
     const [page, setPage] = React.useState<number>(0);
     const [totalPages, setTotalPages] = React.useState<number>(getData(0, selectedState).totalPages);
     const [allBooks, setAllBooks] = React.useState<IBooks[]>(getData(page, selectedState).books);
-    
+    const [nameFilter, setNameFilter] = React.useState<string>('');
+
     function nextBook() {
-        setPage(page+1);
+        setPage(page + 1);
     }
 
     function beforeBook() {
-        setPage(page-1);
+        setPage(page - 1);
+    }
+
+    const changeInput = (e: ChangeEvent<HTMLInputElement>) => {
+        setNameFilter(e.target.value);
     }
 
     React.useEffect(() => {
@@ -36,40 +41,43 @@ export default function Library({books} : LibraryProps) {
     }
 
     return (
-        <div className="flex flex-col gap-10">
-            <div>
 
-                <label htmlFor="select" className="ml-5 font-bold">Filtrado por estado: </label>
-                <select name="select" id="select"
-                value={selectedState}
-                onChange={handleState}>
+        <>
+            <div className="flex flex-col gap-10">
+                <div>
+                    <input onChange={changeInput} value={nameFilter} className="border" />
+
+                    <label htmlFor="select" className="ml-5 font-bold">Filtrado por estado: </label>
+                    <select name="select" id="select"
+                        value={selectedState}
+                        onChange={handleState}>
+                        {
+                            states.map((s, index) => {
+                                return (
+                                    <option key={`state-${s}-${index}`} value={s}>{s}</option>
+                                )
+                            })
+                        }
+                    </select>
+                </div>
+
+                <div className="flex justify-center w-full">
                     {
-                        states.map((s, index) => {
+                        allBooks.map((b, index) => {
                             return (
-                                <option key={`state-${s}-${index}`} value={s}>{s}</option>
+                                <Book key={`book-${b.ISBN}-${index}`} ISBN={b.ISBN} titulo={b.titulo} autor={b.autor} imagen={b.imagen} fechaPublicacion={b.fechaPublicacion} state={b.state} categoria={b.categoria} favourite={b.favourite} />
                             )
                         })
                     }
-                </select>
-            </div>
+                </div>
 
-            <div className="flex justify-center w-full">
-                {
-                    allBooks.map((b, index) => {
-                        return (
-                            <Book key={`book-${b.ISBN}-${index}`} ISBN={b.ISBN} titulo={b.titulo} autor={b.autor} imagen={b.imagen} fechaPublicacion={b.fechaPublicacion} state={b.state} categoria={b.categoria} favourite={b.favourite} />
-                        )
-                    })
-                }
+                <div className="flex flex-row justify-center gap-10 items-center">
+                    {page > 0 && <button onClick={beforeBook} className="py-1 px-3 rounded-2xl font-bold text-white bg-amber-500 hover:bg-amber-400">Before</button>}
+                    <p className="border-2 rounded-full p-2 flex justify-center font-bold bg-amber-100 text-amber-700">{page + 1} de {totalPages}</p>
+                    {page < totalPages - 1 && <button onClick={nextBook} className="py-1 px-3 rounded-2xl font-bold text-white bg-amber-500 hover:bg-amber-400">Next</button>}
+                </div>
             </div>
-
-            <div className="flex flex-row justify-center gap-10 items-center">
-                { page > 0 && <button onClick={beforeBook} className="py-1 px-3 rounded-2xl font-bold text-white bg-amber-500 hover:bg-amber-400">Before</button> }
-                <p className="border-2 rounded-full p-2 flex justify-center font-bold bg-amber-100 text-amber-700">{page+1} de {totalPages}</p>
-                { page < totalPages-1 && <button onClick={nextBook} className="py-1 px-3 rounded-2xl font-bold text-white bg-amber-500 hover:bg-amber-400">Next</button> }
-            </div>
-        </div>
-        
+        </>
     )
 }
 
