13e62dfdbe7702d02ed1509bc2ea33a74b82c0a8
unknown <2DAM-acuelop369@IESMARTINEZM.ES>
2025-11-20 22:04:52 +0100

    Actualización

---
+++ UD3_React_Native/counter-app/app/index.tsx
@@ -0,0 +1,14 @@
+import { Text, View } from "react-native";
+
+
+export default function Index() {
+  return (
+    <View
+      className="flex-1 justify-center items-center"
+    >
+      <Text className="text-pink-600 text-3xl">
+        Bienvenidos a la unidad 3
+      </Text>
+    </View>
+  );
+}
