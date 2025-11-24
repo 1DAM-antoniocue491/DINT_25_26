8154909728393292a9db031e5128281951faa54e
unknown <2DAM-acuelop369@IESMARTINEZM.ES>
2025-11-24 21:57:52 +0100

    Actualización

--- UD3_React_Native/counter-app/app/index.tsx
+++ UD3_React_Native/counter-app/app/index.tsx
@@ -1,14 +1,31 @@
-import { Text, View } from "react-native";
-
+import React from "react";
+import { Pressable, Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import Button from "../components/Button";
 
 export default function Index() {
+
+  const [count, setCount] = React.useState<number>(0);
+
+  function handleButton() {
+    setCount(count+1)
+  }
+
+  function resetCount() {
+    setCount(0)
+  }
+
   return (
-    <View
-      className="flex-1 justify-center items-center"
-    >
-      <Text className="text-pink-600 text-3xl">
-        Bienvenidos a la unidad 3
-      </Text>
-    </View>
+    <SafeAreaView className="flex-1">
+      <View
+        className="relative flex-1 justify-center items-center"
+      >
+        <Text className="text-blue-800 text-9xl">
+          {count}
+        </Text>
+        <Button label="Reset" onPress={resetCount} position="left"></Button>
+        <Button label="+1" onPress={handleButton} onLongPress={resetCount}></Button>
+      </View>
+    </SafeAreaView>
   );
 }
---
+++ UD3_React_Native/counter-app/components/Button.tsx
@@ -0,0 +1,23 @@
+import { View, Text, Pressable } from "react-native";
+
+interface ButtonProps {
+    label: string;
+    onPress?: ()=> void;
+    onLongPress?: ()=> void;
+    position?: "left" | "right";
+}
+
+export default function Button({label, onPress, onLongPress, position="right"}: ButtonProps) {
+    const positionButton = position == "right" ? 'right-10' : 'left-10'
+    return (
+        <Pressable onPress={onPress} onLongPress={onLongPress} delayLongPress={300} className={`absolute ${positionButton} bottom-10`}>
+            {
+                ({pressed}) => (
+                    <View className={`${pressed? 'bg-blue-800/90' : 'bg-blue-800'} p-5 rounded-lg shadow-xl shadow-gray-400`}>
+                        <Text className="text-white font-bold text-xl">{label}</Text>
+                    </View>
+                )
+            }
+        </Pressable>
+    )
+}
